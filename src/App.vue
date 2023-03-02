cc99fb3a32307db1217581e0198027ce279c4951
邓不灵多 <10193888+deng-bulingduo@user.noreply.gitee.com>
2023-03-02 10:00:14 +0800

    03021000

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,63 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <Editor v-model="state.data2"></Editor>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Editor from './packages/editor.jsx'
+import data2 from './data.json'
+import { computed, provide, reactive, ref } from 'vue'
+import {registerConfig} from './utils/editor-config.jsx'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    Editor,
+    // data2
+  },
+  setup(){
+    provide('config',registerConfig)
+
+    const data4 = computed({
+      get(){
+        return data2
+      },
+      set(value){
+        // data2 = JSON.parse(JSON.stringify(value))
+        data2 = value
+      }
+    })
+
+    console.log(data4.value)
+
+    const state = reactive({
+      data2
+    })
+
+    // const data3 = reactive({
+    //   state:data4
+    // })
+    return{
+      // data2,
+      // data3,
+      data4,
+      state
+    }
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+// #app {
+//   font-family: Avenir, Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+//   margin-top: 60px;
+// }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+import { Button } from 'vant';
+import { Tab, Tabs } from 'vant';
+
+// 2. 引入组件样式
+import 'vant/lib/index.css';
+
+const app = createApp(App)
+
+
+app.use(Button);
+app.use(ElementPlus)
+
+app.use(Tab);
+app.use(Tabs);
+app.mount('#app')
