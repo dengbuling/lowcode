07e443b8050c0e79bee6262c0adec69b90df9649
邓不灵多 <10193888+deng-bulingduo@user.noreply.gitee.com>
2023-03-02 14:28:11 +0800

    03021428

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,8 @@
 <script>
 import Editor from "./packages/editor.jsx";
 import { provide, reactive } from "vue";
-import blockData from "./data.json";
 import { registerConfig } from "./utils/editor-config.jsx";
+import blockData from "./data.json";
 
 export default {
     name: "App",
