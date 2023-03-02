6066810a054c00b4c11bf9d1385e701ca9b04c99
邓不灵多 <10193888+deng-bulingduo@user.noreply.gitee.com>
2023-03-02 14:25:58 +0800

    03021425

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,26 @@
 <template>
-  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-  <Editor v-model="state.data2"></Editor>
+    <Editor v-model="state.blockData"></Editor>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-import Editor from './packages/editor.jsx'
-import data2 from './data.json'
-import { computed, provide, reactive, ref } from 'vue'
-import {registerConfig} from './utils/editor-config.jsx'
-
+import Editor from "./packages/editor.jsx";
+import { provide, reactive } from "vue";
+import blockData from "./data.json";
+import { registerConfig } from "./utils/editor-config.jsx";
 
 export default {
-  name: 'App',
-  components: {
-    // HelloWorld,
-    Editor,
-    // data2
-  },
-  setup(){
-    provide('config',registerConfig)
-
-    const data4 = computed({
-      get(){
-        return data2
-      },
-      set(value){
-        // data2 = JSON.parse(JSON.stringify(value))
-        data2 = value
-      }
-    })
-
-    console.log(data4.value)
-
-    const state = reactive({
-      data2
-    })
-
-    // const data3 = reactive({
-    //   state:data4
-    // })
-    return{
-      // data2,
-      // data3,
-      data4,
-      state
-    }
-  }
-}
+    name: "App",
+    components: {
+        Editor,
+    },
+    setup() {
+        provide("config", registerConfig);
+        const state = reactive({
+            blockData,
+        });
+        return {
+            state,
+        };
+    },
+};
 </script>
-
-<style lang="scss">
-// #app {
-//   font-family: Avenir, Helvetica, Arial, sans-serif;
-//   -webkit-font-smoothing: antialiased;
-//   -moz-osx-font-smoothing: grayscale;
-//   text-align: center;
-//   color: #2c3e50;
-//   margin-top: 60px;
-// }
-</style>
