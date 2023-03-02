1c7e3ca5cf2d2f58499cce0bbe9019a469e91b53
邓不灵多 <10193888+deng-bulingduo@user.noreply.gitee.com>
2023-03-02 14:33:57 +0800

    03021433

--- src/main.js
+++ src/main.js
@@ -4,18 +4,16 @@ import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
+// 引入组件样式
+import 'vant/lib/index.css';
+
 import { Button } from 'vant';
 import { Tab, Tabs } from 'vant';
 
-// 2. 引入组件样式
-import 'vant/lib/index.css';
-
 const app = createApp(App)
 
-
-app.use(Button);
 app.use(ElementPlus)
-
+app.use(Button);
 app.use(Tab);
 app.use(Tabs);
 app.mount('#app')
